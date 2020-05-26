82f16b320dcf3c6f929b349728094d2630ec86dc
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2020-05-26 15:01:45 -0700

    Add files via upload

--- numFacts.py
+++ numFacts.py
@@ -1,6 +1,15 @@
 import requests
+import plotly.graph_objects as go
 import json
 
-number = input("Please type a number: ")
-fact = requests.get('http://numbersapi.com/' + ''.join(number) + '?json')
-print(fact.json()["text"])
+for i in range(101):
+    num = i
+    #print(num)
+    fact = requests.get('http://numbersapi.com/' + str(i) + '?json')
+    print(fact.json()["number"])
+
+fig = go.Figure(
+    data=[go.Bar(y=[2, 1, 3], x=['Facts', 'No Facts', 'Extra'])],
+    layout_title_text="A Figure Displayed with fig.show()"
+)
+fig.show()
