f7c615b39bdf95ff504c05e9998072b2606efca9
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2020-05-27 09:52:55 -0500

    Add files via upload

--- numFacts.py
+++ numFacts.py
@@ -1,15 +1,29 @@
 import requests
 import plotly.graph_objects as go
+import time
 import json
 
-for i in range(101):
+facts = 0
+noFacts = 0
+
+
+for i in range(800):
+    numURL = 'http://numbersapi.com/' + str(i) + '?json'
+    pokURL = 'https://pokeapi.co/api/v2/pokemon-species/' + str(1+i)
     num = i
     #print(num)
-    fact = requests.get('http://numbersapi.com/' + str(i) + '?json')
-    print(fact.json()["number"])
+    while True:
+        fact = requests.get(pokURL)
+        try:
+            #print(fact.json()["number"])
+            #print(fact.json()["found"])
+            print(fact.json()["name"].capitalize())
+            break
+        except:
+            pass
 
 fig = go.Figure(
-    data=[go.Bar(y=[2, 1, 3], x=['Facts', 'No Facts', 'Extra'])],
+    data=[go.Bar(y=[fact, noFacts], x=['Facts', 'No Facts'])],
     layout_title_text="A Figure Displayed with fig.show()"
 )
 fig.show()
